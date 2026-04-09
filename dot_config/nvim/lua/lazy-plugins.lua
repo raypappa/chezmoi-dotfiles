8b75a6acf567a4be4382d94dbd13ee2d05d414c7
Craig Davis <craig@pienso.com>
2026-04-09 08:31:04 -0700

    add missing grug-far from require

--- dot_config/nvim/lua/lazy-plugins.lua
+++ dot_config/nvim/lua/lazy-plugins.lua
@@ -52,17 +52,11 @@ require('lazy').setup({
 
   require 'kickstart/plugins/lint',
 
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- put them in the right spots if you want.
+  require 'kickstart/plugins/grug-far',
 
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for kickstart
-  --
-  --  Here are some example plugins that I've included in the kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
+  require 'kickstart/plugins/indent_line',
+
   require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
