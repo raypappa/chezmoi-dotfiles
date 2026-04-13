7223550c04fb7fd979d5ae9f91626c124260d8aa
Craig Davis <craig@pienso.com>
2026-04-13 08:43:21 -0700

    replace glow with render-markdown

--- dot_config/nvim/lua/custom/plugins/glow.lua
+++ dot_config/nvim/lua/custom/plugins/glow.lua
@@ -1,1 +1,22 @@
-return { 'ellisonleao/glow.nvim', config = true, cmd = 'Glow' }
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      anti_conceal = {
+        enabled = true,
+        above = 2,
+        below = 2,
+      },
+    },
+  },
+  -- {
+  --   'ellisonleao/glow.nvim',
+  --   config = true,
+  --   cmd = 'Glow',
+  -- },
+}
