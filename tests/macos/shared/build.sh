a27e7230e1a5491aba7414d7183506d4feab0cc3
Craig Davis <craig@pienso.com>
2026-04-14 11:45:48 -0700

    switch back to using sh so it's installed in the correct location

--- tests/macos/shared/build.sh
+++ tests/macos/shared/build.sh
@@ -15,7 +15,7 @@ cat << 'EOF' > ~/.config/chezmoi/chezmoi.toml
 [env]
 GH_SKIP = "1"
 EOF
-zsh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init -R=always --use-builtin-git true --apply https://github.com/raypappa/chezmoi-dotfiles.git
+sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- -b $HOME/.local/bin init -R=always --use-builtin-git true --apply https://github.com/raypappa/chezmoi-dotfiles.git
 
 echo "Finished installing dotfiles"
 
