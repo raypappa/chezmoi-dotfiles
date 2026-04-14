fbfc6906447695231afda8cbb2f8a813c0c2b8f8
Craig Davis <craig@pienso.com>
2026-04-14 12:31:11 -0700

    use whence over which

--- tests/macos/shared/tests.sh
+++ tests/macos/shared/tests.sh
@@ -2,18 +2,18 @@
 set -e
 set -x
 
-test "$(which sed)" = /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed
-test "$(which make)" = /opt/homebrew/opt/make/libexec/gnubin/make
-test "$(which patch )" = /opt/homebrew/opt/gpatch/libexec/gnubin/patch
-test "$(which time)" = /opt/homebrew/opt/gnu-time/libexec/gnubin/time
-test "$(which tar)" = /opt/homebrew/opt/gnu-tar/libexec/gnubin/tar
-test "$(which awk)" = /opt/homebrew/opt/gawj/libexec/gnubin/awk
-test "$(which find)" = /opt/homebrew/opt/findutils/libexec/gnubin/find
-test "$(which date)" = /opt/homebrew/opt/coreutils/libexec/gnubin/date
-test "$(which base64)" = /opt/homebrew/coreutils/libexec/gnubin/base64
-test "$(which dd)" = /opt/homebrew/opt/coreutils/libexec/gnubin/dd
-test "$(which tee)" = /opt/homebrew/opt/coreutils/libexec/gnubin/tee
-test "$(which tr)" = /opt/homebrew/opt/coreutils/libexec/gnubin/tr
+test "$(whence -p sed)" = /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed
+test "$(whence -p make)" = /opt/homebrew/opt/make/libexec/gnubin/make
+test "$(whence -p patch )" = /opt/homebrew/opt/gpatch/libexec/gnubin/patch
+test "$(whence -p time)" = /opt/homebrew/opt/gnu-time/libexec/gnubin/time
+test "$(whence -p tar)" = /opt/homebrew/opt/gnu-tar/libexec/gnubin/tar
+test "$(whence -p awk)" = /opt/homebrew/opt/gawj/libexec/gnubin/awk
+test "$(whence -p find)" = /opt/homebrew/opt/findutils/libexec/gnubin/find
+test "$(whence -p date)" = /opt/homebrew/opt/coreutils/libexec/gnubin/date
+test "$(whence -p base64)" = /opt/homebrew/coreutils/libexec/gnubin/base64
+test "$(whence -p dd)" = /opt/homebrew/opt/coreutils/libexec/gnubin/dd
+test "$(whence -p tee)" = /opt/homebrew/opt/coreutils/libexec/gnubin/tee
+test "$(whence -p tr)" = /opt/homebrew/opt/coreutils/libexec/gnubin/tr
 test "$(whence -p mise)" = $HOME/.local/bin/mise
 mise doctor
 brew doctor
